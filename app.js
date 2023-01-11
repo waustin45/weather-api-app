ca28202d64950d7a205002bb2d2713cb1d958773
waustin45 <108015666+waustin45@users.noreply.github.com>
2023-01-11 17:51:23 -0500

    commit made redactions added parameters

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const apieKey = '8df524fcfcbbb5a966a64726ff3d0434'
 const inputBar = document.querySelector('#input')
 const inputValue = inputBar.value
 console.log(inputValue)
-let long = 0,
-     lat = 0;
 const cityList = document.querySelector('.searched-cities')
 const cardDiv = document.querySelector('.card-div')
 const topInfo = document.querySelector('.top-info')
@@ -22,18 +20,21 @@ let thisDay = dayjs().format('DD')
 // array to hld the searched cities
 let citiesArray = [];
 //function to add searched cities to local storage
-searchBtn.addEventListener('click', saveSearch)
-searchBtn.addEventListener('click' , fetchData)
-function saveSearch () {
-    const value = inputBar.value
-    
+
+searchBtn.addEventListener('click' , () => {
+    const item = inputBar.value
+    fetchData(item)
+    saveSearch(item)
+})
+function saveSearch (value) {
     if(localStorage.getItem('cities')) {
         citiesArray = JSON.parse(localStorage.getItem('cities'))
        console.log(citiesArray + "in if statement")
 
-   } else {
+    } else {
        citiesArray = []
-   }
+       
+    }
     citiesArray.push(value)
     const newCitiesMap =  citiesArray.map(info => {
         return `<li><button class="searched-city-btn new">${info}</button></li>`
@@ -42,8 +43,10 @@ function saveSearch () {
         cityList.innerHTML = newCitiesMap
     }else {
         citiesArray =[]
+       
     }
     
+    fetchSearchedData()
     console.log(citiesArray)
     localStorage.setItem('cities', JSON.stringify(citiesArray))
 }
@@ -63,42 +66,43 @@ function showSearches () {
 }
 showSearches()
 // functionto fetch weather datat from api
-function fetchData () {
-    
-    const item = inputBar.value
+function fetchData (item) {
     const urlForCity = `https://api.openweathermap.org/geo/1.0/direct?q=${item}&limit=1&appid=${apieKey}`
     fetch(urlForCity).then(res => res.json()).then(data => {
         console.log(data)
-        lat = data[0].lat
-        long = data[0].lon
+        let lat = data[0].lat
+        let long = data[0].lon
+        let name= data[0].name
         console.log("lat " + lat, "lon " + long)
         // fetch function for weather data 
-        function weatherData () {
-            const urlForLatLong = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&cnt=5&exclude=alerts&appid=${apieKey}`
-            fetch(urlForLatLong).then(res => res.json()).then(weather => {
-                console.log(weather)
-                infoHead.innerHTML = data[0].name + " " + currentDay + " " + `<img src=https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png></img>`
-                temp.innerHTML = " " + weather.current.temp + "ºF"
-                wind.innerHTML = " " + weather.current.wind_speed + " MPH"
-                humidity.innerHTML = " " + weather.current.humidity + " %"
-                
-               cardDiv.innerHTML = weather.daily.splice(1, 5).map( info => {
-                //converts UTC time to local time.
-                    const timestamp = info.dt
-                    const date = new Date(timestamp * 1000)
-                    return `<div class="card">
-                        <h3>${date.toLocaleString("en-US").slice(0, 9)}</h3>
-                        <img src="https://openweathermap.org/img/wn/${info.weather[0].icon}.png"></img>
-                        <div> Temp: <span>${info.temp.day}ºF</span></div>
-                        <div> Wind: <span>${info.wind_speed}MPH</span></div>
-                        <div> Humidity: <span>${info.humidity}%</span></div>
-                    </div>`
-                }).join('')
-            })
-        }
-        weatherData()
+       weatherData(lat, long, name)
     })
 }
+
+function weatherData (lat, long, name) {
+    const urlForLatLong = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&cnt=5&exclude=alerts&appid=${apieKey}`
+    fetch(urlForLatLong).then(res => res.json()).then(weather => {
+        console.log(weather)
+        infoHead.innerHTML = name + " " + currentDay + " " + `<img src=https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png></img>`
+        temp.innerHTML = " " + weather.current.temp + "ºF"
+        wind.innerHTML = " " + weather.current.wind_speed + " MPH"
+        humidity.innerHTML = " " + weather.current.humidity + " %"
+        
+       cardDiv.innerHTML = weather.daily.splice(1, 5).map( info => {
+        //converts UTC time to local time.
+            const timestamp = info.dt
+            const date = new Date(timestamp * 1000)
+            return `<div class="card">
+                <h3>${date.toLocaleString("en-US").slice(0, 9)}</h3>
+                <img src="https://openweathermap.org/img/wn/${info.weather[0].icon}.png"></img>
+                <div> Temp: <span>${info.temp.day}ºF</span></div>
+                <div> Wind: <span>${info.wind_speed}MPH</span></div>
+                <div> Humidity: <span>${info.humidity}%</span></div>
+            </div>`
+        }).join('')
+    })
+}
+
 // fetches weather data for searched items. press searched item to trigger
 function fetchSearchedData () {
    
@@ -107,40 +111,7 @@ function fetchSearchedData () {
     searchedBtn.forEach(each => {
         const buttonItem = each.textContent
         console.log(buttonItem)
-        each.addEventListener('click', () => {
-            const urlForCity = `https://api.openweathermap.org/geo/1.0/direct?q=${buttonItem}&limit=1&appid=${apieKey}`
-            fetch(urlForCity).then(res => res.json()).then(data => {
-                console.log(data)
-                lat = data[0].lat
-                long = data[0].lon
-                console.log("lat " + lat, "lon " + long)
-                // fetch function for weather data 
-                function weatherData () {
-                    const urlForLatLong = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&cnt=5&appid=${apieKey}`
-                    fetch(urlForLatLong).then(res => res.json()).then(weather => {
-                        console.log(weather)
-                        infoHead.innerHTML = data[0].name + " " + currentDay + " " + `<img src=https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png></img>`
-                        temp.innerHTML = " " + weather.current.temp + "ºF"
-                        wind.innerHTML = " " + weather.current.wind_speed + " MPH"
-                        humidity.innerHTML = " " + weather.current.humidity + " %"
-                        console.log(JSON.parse(thisDay) + 5)
-                        cardDiv.innerHTML = weather.daily.splice(1, 5).map( info => {
-                            //converts UTC time to local time.
-                            const timestamp = info.dt
-                            const date = new Date(timestamp * 1000)
-                            return `<div class="card">
-                                <h3>${date.toLocaleString("en-US").slice(0, 9)}</h3>
-                                <img src="https://openweathermap.org/img/wn/${info.weather[0].icon}.png"></img>
-                                <div> Temp: <span>${info.temp.day}ºF</span></div>
-                                <div> Wind: <span>${info.wind_speed}MPH</span></div>
-                                <div> Humidity: <span>${info.humidity}%</span></div>
-                            </div>`
-                        }).join('')
-                    })
-                }
-                weatherData()
-            })
-        })
+        each.addEventListener('click', () => {fetchData(buttonItem)})
     })
 }
 fetchSearchedData()
